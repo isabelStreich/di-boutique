4b0ebc1a8806519d0aba1e840df324c9a0453043
Dmytro <dlunhu@gmail.com>
2020-05-18 19:19:35 -0400

    Produit Controler

--- DI-boutique/src/main/java/controler/ProduitControler.java
+++ DI-boutique/src/main/java/controler/ProduitControler.java
@@ -5,13 +5,16 @@
  */
 package controler;
 
+import entities.Produit;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import manager.ProduitManager;
 
 /**
  *
@@ -31,19 +34,40 @@ public class ProduitControler extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ProduitControler</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ProduitControler at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        String url = "";
+        // 1 recuperation
+        String idCat = request.getParameter("idCat");
+        String id = request.getParameter("id");
+        String action = request.getParameter("action");
+
+        //2 traitement
+        if (idCat != null) { // affiche les animaux par categorie
+            ArrayList<Produit> produits = ProduitManager.getByIdCat(Integer.parseInt(idCat));
+            request.setAttribute("listProduit", produits);
+            url = "produits.jsp";
+        } else if (id != null) { //affiche un idProduit par id
+            int idProduit = Integer.parseInt(id);
+            ArrayList<Produit> produit = ProduitManager.getByIdCat(idProduit);
+            if (action != null) {
+                //ajouter un j aime au manager
+                //recuperer produit par son iid
+                //j aoute un j aime pour ce produit qui cet Id
+                JaimeProduitManager.add(produit.getNom());
+            }
+            request.setAttribute("produit", produit);
+            url = "produit.jsp";
+        } else { // affiche tous les animaux
+            ArrayList<Produit> produits = ProduitManager.getAll();
+            request.setAttribute("listProduit", produits);
+            url = "produits.jsp";
         }
+        //ajouter les categories dans la requette
+        request.setAttribute("listCategorie", CategorieManager.getAll());
+        //doit ajouter le hashMapde jaime a la requette
+        request.setAttribute("nbJaime", JaimeProduitManager.getNombreDeJaime());
+
+        //3 redirection
+        request.getRequestDispatcher(url).forward(request, response);
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
--- DI-boutique/src/main/java/manager/ProduitManager.java
+++ DI-boutique/src/main/java/manager/ProduitManager.java
@@ -19,36 +19,48 @@ import service.ConnectionBD;
  * @author istreich
  */
 public class ProduitManager {
+
+//Dmytro debut
+    public static ArrayList<Produit> getByIdCat(int idCat) {
+        ArrayList<Produit> retour = new ArrayList<>();
+        Iterable<Produit> produits = null;
+        for (Produit produit : produits) {
+            if (produit.getIdCategorie() == idCat) {
+                retour.add(produit);
+            }
+        }
+        return retour;
+    }
+//Dmytro fin
+
     private static String queryGetAll = "select * from produit";
-    
-        public static ArrayList<Produit> getAll() {
-        
-         ArrayList<Produit> retour = null;
-         
-         PreparedStatement ps = ConnectionBD.getPs(queryGetAll);
-         
+
+    public static ArrayList<Produit> getAll() {
+
+        ArrayList<Produit> retour = null;
+
+        PreparedStatement ps = ConnectionBD.getPs(queryGetAll);
+
         try {
             ResultSet result = ps.executeQuery();
-            
-             if(result.isBeforeFirst()){
-                 retour = new ArrayList<>();
-                 while (result.next()){
+
+            if (result.isBeforeFirst()) {
+                retour = new ArrayList<>();
+                while (result.next()) {
 //                 Produit p = new Produit();
 //                 p.setIdProduit(result.getInt("idProduit"));
 //                 p.setNomProduit(result.getString("nomProduit"));
 //                 p.setIdCategorie(result.getString("idCategorie"));
-                     
-                 }
-         }
-            
-            
+
+                }
+            }
+
         } catch (SQLException ex) {
             Logger.getLogger(ProduitManager.class.getName()).log(Level.SEVERE, null, ex);
         }
-         
-         ConnectionBD.close();
-         return retour;
-        }
 
-    
+        ConnectionBD.close();
+        return retour;
+    }
+
 }
